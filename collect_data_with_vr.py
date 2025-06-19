fa4def3c46bffdc69525daf8d46d11974136c768
RiadAhmed-prog <riadrmedu.gmail.com>
2025-06-19 12:05:56 -0400

    wrote collection script for vr

--- collect_data_with_vr.py
+++ collect_data_with_vr.py
@@ -1,50 +1,38 @@
-"""Teleoperating robot arm with a SpaceMouse to collect demonstration data"""
-
 import argparse
-import json
-import os
-import pickle
-import threading
 import time
-from pathlib import Path
-
-import matplotlib.pyplot as plt
-import numpy as np
-# from rpl_vision_utils.networking.camera_redis_interface import \
-#     CameraRedisSubInterface
-from oculus_reader.reader import OculusReader
-
-from deoxys_vision.networking.camera_redis_interface import CameraRedisSubInterface
-from deoxys_vision.camera.rs_interface import RSInterface
-from deoxys_vision.utils.img_utils import preprocess_color, preprocess_depth
+import os
 from deoxys_vision.utils.camera_utils import assert_camera_ref_convention, get_camera_info
 
 from deoxys import config_root
 from deoxys.franka_interface import FrankaInterface
-from deoxys.utils import YamlConfig
-from deoxys.utils.config_utils import robot_config_parse_args
+from deoxys.utils.config_utils import get_default_controller_config
 from deoxys.utils.input_utils import input2action
-# from deoxys.k4a_interface import K4aInterface
 from deoxys.utils.io_devices import SpaceMouse
 from deoxys.utils.log_utils import get_deoxys_example_logger
-import cv2 
+import matplotlib.pyplot as plt
+import numpy as np
+from oculus_reader.reader import OculusReader
+from scipy.spatial.transform import Rotation as R
+from deoxys_vision.networking.camera_redis_interface import CameraRedisSubInterface
 from threading import Thread
+import json
 
 logger = get_deoxys_example_logger()
-
 
 import beepy as beep
 beep_start = lambda : beep.beep('coin')
 beep_end= lambda : beep.beep('ready')
 
-
+###########################################################
+# Funtion read read parse and return command line arguemnts
+###########################################################
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--interface-cfg", type=str, default="charmander.yml")
     parser.add_argument("--folder", type=Path, default="/home/franka_deoxys/data_franka/imgsd_demo/")
     parser.add_argument("--controller-type", type=str, default="OSC_POSE")
     parser.add_argument(
-        "--controller-cfg", type=str, default="osc-position-controller.yml"
+        "--controller-cfg", type=str, default="osc-pose-controller.yml"
     )
     
     parser.add_argument("--use-depth", action="store_true", 
@@ -63,10 +51,15 @@ def parse_args():
     parser.add_argument(
         "--product_id",
         type=int,
-        default=50734,
+        default=50746,   #  50770.,  50746
     )
     # robot_config_parse_args(parser)
     return parser.parse_args()
+
+###########################################################
+# Function to parse the oculus reader state inputs to the
+# 7 dimensional action input for the franka
+###########################################################
 
 def input_to_action(state, last_state):
     state_pose_data, state_input_data = state
@@ -74,18 +67,19 @@ def input_to_action(state, last_state):
 
     # Skip if either part is empty
     if not state_pose_data or not last_pose_data:
-        return None
+        return None, None
 
     pose1 = last_pose_data['r']
     pose2 = state_pose_data['r']
+
+    np.set_printoptions(precision=2)
+
     delta_pose = np.linalg.inv(pose1) @ pose2
+
     translation = delta_pose[:3, 3]
     rotation_matrix = delta_pose[:3, :3]
     rotation = R.from_matrix(rotation_matrix).as_euler('xyz')
 
-    # joystick_delta = np.array(state_input_data['rightJS']) - np.array(last_input_data['rightJS'])
-    # trigger_delta = np.array(state_input_data['rightTrig']) - np.array(last_input_data['rightTrig'])
-    
     if(state_input_data['rightTrig'][0] > 0):
         trigger_delta = 1
     else:
@@ -96,19 +90,72 @@ def input_to_action(state, last_state):
     else:
         grip_delta = -1
 
-    return {
+    rotation_scale = 3
+    translation_scale = 70
+
+    rotation = [
+        rotation[1] * rotation_scale, # Z rotation
+        rotation[0] * rotation_scale * 0.8, # X rotation
+        -rotation[2] * rotation_scale # Y rotation
+    ]
+
+    translation = [
+        translation[1] * translation_scale / 2 * 5, # Z / forward backward
+        translation[0] * translation_scale,  # X / left right
+        -translation[2] * translation_scale # Y / up down
+    ]
+
+
+    return ({
         "delta_pos": translation,
         "delta_rot": rotation,
         # "joystick": joystick_delta,
         "trigger": [trigger_delta],
         "grip": [grip_delta]
-    }, [np.array(translation + rotation) , [trigger_delta]]
+    }, np.array([*translation, *rotation, trigger_delta]))
+
+###########################################################
+# Read vr controller state and perform logic controls
+# and state manipulation logic
+############################################################
+
+def read_vr_action(oculus_reader, last_state, last_trigger):
+    empty_action_data = {
+        "delta_pos": [0.0,0.0,0.0],
+        "delta_rot": [0.0,0.0,0.0],
+        "trigger": [0.0],
+        "grip": [0.0]
+    }
+    empty_action = np.array([0., 0., 0., 0., 0., 0., -1.0])
+
+
+    empty_action[6] = last_trigger
+    
+    state = oculus_reader.get_transformations_and_buttons()
+    
+    action = empty_action
+    input_action_data = empty_action_data
+    if last_state is not None:
+        input_action_data, input_action = input_to_action(state, last_state)
+        if input_action_data is not None:
+            if input_action_data['grip'][0] > 0:
+                action = input_action
+                last_trigger = input_action[6]
+                # pass
+    last_state = state
+
+    return action, last_state, last_trigger
+
+
+###########################################################
+# Main
+############################################################
 
 def main():
-    args = parse_args()
 
+    args = parse_args()
     use_depth = args.use_depth
-    horizon = args.horizon 
+    horizon = args.horizon
 
     print(f'-----------collecting data with {horizon} max steps')
 
@@ -116,9 +163,11 @@ def main():
 
     experiment_id = 0
 
-    logger.info(f"Saving to {args.folder}")
-
+    ###########################################################
     # Create a folder that saves the demonstration raw states.
+    ###########################################################
+    
+    logger.info(f"Saving to {args.folder}")
     for path in args.folder.glob("run*"):
         if not path.is_dir():
             continue
@@ -131,20 +180,23 @@ def main():
     experiment_id += 1
     folder = str(args.folder / f"run{experiment_id}")
 
-    oculus_reader = OculusReader()
+    ###########################################################
+    # Setup Franka interface
+    ###########################################################
 
     cfg_file = os.path.join(config_root, args.interface_cfg)
     print('cfg_file=', cfg_file)
 
-    # Franka Interface
     robot_interface = FrankaInterface(cfg_file)
+    robot_interface._state_buffer = []
 
+    ###########################################################
+    # Setup Cameras
+    ###########################################################
+    
     camera_ids = [0, 1]
     # camera_ids = [0]
     cr_interfaces = {}
-
-
-
 
     for camera_id in camera_ids:
         camera_ref=f"rs_{camera_id}"
@@ -160,12 +212,9 @@ def main():
         cr_interface.start()
         cr_interfaces[camera_id] = cr_interface
 
-    controller_cfg = YamlConfig(
-        os.path.join(config_root, args.controller_cfg)
-    ).as_easydict()
-
-    # demo_file = h5py.File(demo_file_name, "w")
     controller_type = args.controller_type
+    controller_cfg = get_default_controller_config(controller_type=controller_type)
+    # controller_cfg = YamlConfig("config/osc-pose-controller.yml").as_easydict()
 
     data = {"action": [], "ee_states": [], "joint_states": [], "gripper_states": []}
     for camera_id in camera_ids:
@@ -174,84 +223,52 @@ def main():
         data[f"camera_{camera_id}_depth"] = []
 
 
-
+    ###########################################################
+    # Demo Collection
+    ###########################################################
     i = 0
     start = False
 
     previous_state_dict = None
-
-
-    # beep_start()
+    beep_start()
     time.sleep(2)
 
     print('------------------------record--------now----------------------')
     tb=Thread(target=beep_start)
     tb.start()
+
+    ###########################################################
+    # Move Robot
+    ###########################################################
+    
+    oculus_reader = OculusReader()
     last_state = None
-
-
-    empty_action_data = {
-        "delta_pos": [0.0,0.0,0.0],
-        "delta_rot": [0.0,0.0,0.0],
-        "trigger": [0.0],
-        "grip": [0.0]
-    }
-
-    empty_action = np.array([0., 0., 0., 0., 0., 0., -1.0])
-
-
+    last_trigger = -1.0
     while i < horizon:
         i += 1
         start_time = time.time_ns()
-        
-        state = oculus_reader.get_transformations_and_buttons()
-        print("STATE: ", state)
-        action = empty_action
-        input_action_data = empty_action_data
-        if last_state is not None:
-            input_action_data, input_action = input_to_action(state, last_state)
-            if input_action_data is not None:
-                if input_action_data['grip'][0] > 0:
-                    # action = input_action
-                    pass
-        last_state = state
-        grasp = input_action_data['trigger']
+        action, last_state, last_trigger = read_vr_action(oculus_reader, last_state, last_trigger)
 
-        if action is None:
-            break
-
-        # set unused orientation dims to 0
-        if controller_type == "OSC_YAW":
-            action[3:5] = 0.0
-        elif controller_type == "OSC_POSITION":
-            action[3:6] = 0.0
-        
-        print("ROBOT ACTION: ", action)
         robot_interface.control(
             controller_type=controller_type,
             action=action,
             controller_cfg=controller_cfg,
         )
-        print("contolled")
 
-        if len(robot_interface._state_buffer) == 0:
-            continue
-        last_state = robot_interface._state_buffer[-1]
-        last_gripper_state = robot_interface._gripper_state_buffer[-1]
-        if np.linalg.norm(action[:-1]) < 1e-3 and not start:
-            continue
+        ###########################################################
+        # Record states
+        ###########################################################
 
         start = True
-        # print(action.shape)
-        # Record ee pose,  joints
 
         data["action"].append(action)
-
         state_dict = {
             "ee_states": np.array(last_state.O_T_EE),
             "joint_states": np.array(last_state.q),
-            "gripper_states": np.array(last_gripper_state.width),
+            "gripper_states": np.array(last_trigger),
         }
+
+
 
         if previous_state_dict is not None:
             for proprio_key in state_dict.keys():
@@ -263,88 +280,90 @@ def main():
             data[proprio_key].append(state_dict[proprio_key])
 
         previous_state_dict = state_dict
-        # data["ee_states"].append(np.array(last_state.O_T_EE))
-        # joints = np.array(last_state.q)
-        # if np.sum(np.abs(joints)) < 1e-6:
-        #     print("Joints missing!!!!")
-        # data["joint_states"].append(np.array(last_state.q))
-        # data["gripper_states"].append(np.array(last_gripper_state.width))
-        # Get img info
 
-        # for camera_id in camera_ids:
-        #     img_info = cr_interfaces[camera_id].get_img_info()
-        #     data[f"camera_{camera_id}"].append(img_info)
+        for camera_id in camera_ids:
+            try:
+                img_info = cr_interfaces[camera_id].get_img_info()
+                data[f"camera_{camera_id}"].append(img_info)
 
-        #     imgs = cr_interfaces[camera_id].get_img()
-        #     # print('keys: ', imgs.keys())
-        #     color_img = imgs["color"][..., ::-1]
-        #     color_img = cv2.resize(color_img, None, fx=0.5, fy=0.5)
-        #     data[f"camera_{camera_id}_color"].append(color_img)
+                imgs = cr_interfaces[camera_id].get_img()
+                color_img = imgs["color"][..., ::-1]
+                color_img = cv2.resize(color_img, None, fx=0.5, fy=0.5)
+                data[f"camera_{camera_id}_color"].append(color_img)
 
-        #     if use_depth:
-        #         depth_img = imgs["depth"]
-        #         depth_img = cv2.resize(depth_img, None, fx=0.5, fy=0.5)
-        #         data[f"camera_{camera_id}_depth"].append(depth_img)
-            # cv2.imshow(f'color_img_{camera_id}', color_img)
-            # cv2.waitKey(1)
-
-            # color_img = cv2.resize(color_img, (224, 224))  
-            # depth_img = cv2.resize(depth_img, (224, 224))
-
-            # scale by half.
-            
-        
-
-        # TODO: Test if we can directly save img (probably not)
-        # img = cr_interface.get_img() 
-
+                if use_depth:
+                    depth_img = imgs["depth"]
+                    depth_img = cv2.resize(depth_img, None, fx=0.5, fy=0.5)
+                    data[f"camera_{camera_id}_depth"].append(depth_img)
+            except Exception as e:
+                logger.error(f"Error with camera {camera_id}: {e}")
+                continue
 
         end_time = time.time_ns()
         print(f"Time profile: {(end_time - start_time) / 10 ** 9} steps: {i}/{horizon}")
 
-    # cv2.destroyAllWindows()
-        
-    # os.makedirs(folder, exist_ok=True)
-    # with open(f"{folder}/config.json", "w") as f:
-    #     config_dict = {
-    #         "controller_cfg": dict(controller_cfg),
-    #         "controller_type": controller_type,
-    #     }
-    #     json.dump(config_dict, f)
-    #     np.savez(f"{folder}/testing_demo_action", data=np.array(data["action"]))
-    #     np.savez(f"{folder}/testing_demo_ee_states", data=np.array(data["ee_states"]))
-    #     np.savez(
-    #         f"{folder}/testing_demo_joint_states", data=np.array(data["joint_states"])
-    #     )
-    #     np.savez(
-    #         f"{folder}/testing_demo_gripper_states",
-    #         data=np.array(data["gripper_states"]),
-    #     )
-
-    # for camera_id in camera_ids:
-    #     np.savez(
-    #         f"{folder}/testing_demo_camera_{camera_id}_color",
-    #         data=np.array(data[f"camera_{camera_id}_color"]),
-    #     )
-    #     if use_depth:
-    #         np.savez(
-    #             f"{folder}/testing_demo_camera_{camera_id}_depth",
-    #             data=np.array(data[f"camera_{camera_id}_depth"]),
-    #         )
-
-
-    # for camera_id in camera_ids:
-    #     np.savez(
-    #         f"{folder}/testing_demo_camera_{camera_id}",
-    #         data=np.array(data[f"camera_{camera_id}"]),
-    #     )
-    #     cr_interfaces[camera_id].stop()
-    # robot_interface.close()
-    # print("Total length of the trajectory: ", len(data["action"]))
-    # # beep_end()
-    # tb=Thread(target=beep_end)
-    # tb.start()
+    ###########################################################
+    # Save recorded states to file
+    ###########################################################
     
+    os.makedirs(folder, exist_ok=True)
+    with open(f"{folder}/config.json", "w") as f:
+        config_dict = {
+            "controller_cfg": dict(controller_cfg),
+            "controller_type": controller_type,
+        }
+        json.dump(config_dict, f)
+        np.savez(f"{folder}/testing_demo_action", data=np.array(data["action"]))
+        np.savez(f"{folder}/testing_demo_ee_states", data=np.array(data["ee_states"]))
+        np.savez(
+            f"{folder}/testing_demo_joint_states", data=np.array(data["joint_states"])
+        )
+        np.savez(
+            f"{folder}/testing_demo_gripper_states",
+            data=np.array(data["gripper_states"]),
+        )
+
+    for camera_id in camera_ids:
+        np.savez(
+            f"{folder}/testing_demo_camera_{camera_id}_color",
+            data=np.array(data[f"camera_{camera_id}_color"]),
+        )
+        if use_depth:
+            np.savez(
+                f"{folder}/testing_demo_camera_{camera_id}_depth",
+                data=np.array(data[f"camera_{camera_id}_depth"]),
+            )
+
+
+    for camera_id in camera_ids:
+        np.savez(
+            f"{folder}/testing_demo_camera_{camera_id}",
+            data=np.array(data[f"camera_{camera_id}"]),
+        )
+        cr_interfaces[camera_id].stop()
+    robot_interface.close()
+
+    ###########################################################
+    # Clean up
+    ###########################################################
+    
+    print("Total length of the trajectory: ", len(data["action"]))
+    # beep_end()
+    tb=Thread(target=beep_end)
+    tb.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
